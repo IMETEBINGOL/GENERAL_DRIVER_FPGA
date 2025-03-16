7f8ba8cc8041ee9093e654d70da018108a53f5d3
IMETEBINGOL <mete.bingol@metu.edu.tr>
2025-03-17 00:38:15 +0300

    SPI and SPI HANDLER are updated.

--- FIRMWARE/SIM/COMMUNICATION_AND_CONTROL/SPI/SPI_COMMAND_HANDLER/TESTBENCH/spi_command_handler_tb.v
+++ FIRMWARE/SIM/COMMUNICATION_AND_CONTROL/SPI/SPI_COMMAND_HANDLER/TESTBENCH/spi_command_handler_tb.v
@@ -17,28 +17,98 @@ localparam  SECOND_IN_NANOSECOND    = 1_000_000_000;
 localparam  CLK_PERIOD              = SECOND_IN_NANOSECOND/CAC_CLK_FREQUENCY;
 // ---
 
+reg     [CAC_SPI_PACKAGE_WIDTH-1:0]     cmd         = DRST;
+reg     [CAC_SPI_PACKAGE_WIDTH-1:0]     data_in     = DRST;
+reg     [CAC_SPI_PACKAGE_WIDTH-2:0]     addr_in     = DRST;
+reg                                     exec        = LOW;
+reg                                     sdi         = LOW;
+
 // CLOCK GENERATION
 wire        clk;
 // ---
 clock_generation_sim #(
-    .CLK_1_FREQUENCY(CAC_CLK_FREQUENCY),
-    .CLK_1_INITIALIZING_DELAY(0),
-    .CLK_1_PHASE(0)
+    .CLK_1_FREQUENCY                    (CAC_CLK_FREQUENCY),
+    .CLK_1_INITIALIZING_DELAY           (0),
+    .CLK_1_PHASE                        (0)
 ) 
 CLOCK_GENERATION
 (
-    .clk_out_1(clk)
+    .clk_out_1                          (clk)
 );
 // ---
 
 // RESET GENERATION 
-reg     rstb    = HIGH;
+reg                                     rstb    = HIGH;
 // ---
 initial
 begin
     rstb        = LOW;
     #(10*CLK_PERIOD);
     rstb        = HIGH;
+end
+// ---
+
+// SPI COMMAND HANDLER
+wire                                busy;
+wire [CAC_SPI_PACKAGE_WIDTH-1:0]    data_out;
+wire                                csb;
+wire                                sdo;
+// ---
+spi_command_handler #(
+    .PACKAGE_SIZE                       (CAC_SPI_PACKAGE_WIDTH)
+) 
+UUT
+(
+    .clk                                (clk),
+    .rstb                               (rstb),
+    .cmd                                (cmd),
+    .data_in                            (data_in),
+    .addr_in                            (addr_in),
+    .exec                               (exec),
+    .busy                               (busy),
+    .data_out                           (data_out),
+    .sdi                                (sdi),
+    .csb                                (csb),
+    .sdo                                (sdo)
+);
+// ---
+
+
+// TEST 
+// ---
+initial
+begin
+    #(20*CLK_PERIOD);
+    @(posedge clk);
+    cmd         = 8'h01;        // WRITE
+    addr_in     = 8'h05;
+    data_in     = 8'h27;
+    exec        = HIGH;
+    @(posedge clk)
+    exec        = LOW;
+    @(posedge csb);
+    #(20*CLK_PERIOD);
+    @(posedge clk);
+    cmd         = 8'h02;        // READ
+    addr_in     = 8'h07;
+    exec        = HIGH;
+    @(negedge csb)
+    exec        = LOW;
+    sdi         = HIGH;
+    repeat(16)
+    begin
+        @(negedge clk);
+        sdi     = !sdi; 
+    end
+    if (data_out == 8'haa)
+    begin
+        $display("TEST PASSED.");
+    end
+    else
+    begin
+        $display("TEST FAILED.");
+    end
+    $finish;
 end
 // ---
 
--- FIRMWARE/SIM/COMMUNICATION_AND_CONTROL/SPI/SPI_COMMAND_HANDLER/wave.tcl
+++ FIRMWARE/SIM/COMMUNICATION_AND_CONTROL/SPI/SPI_COMMAND_HANDLER/wave.tcl
@@ -1,3 +1,5 @@
 create_wave_config  wave.wcfg
-add_wave_group SPI
-add_wave -into SPI  /spi_command_handler_tb/UUT/*
+add_wave_group SPI_COMMAND_HANDLER
+add_wave_group SPI_DRIVER
+add_wave -into SPI_COMMAND_HANDLER  /spi_command_handler_tb/UUT/*
+add_wave -into SPI_DRIVER  /spi_command_handler_tb/UUT/SPI_INSTANTATION/*
